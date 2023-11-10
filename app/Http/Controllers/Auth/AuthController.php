bce88f84d5096a73a07b9532b01bfb118b5e87f5
yuya671023 <126576075+yuya671023@users.noreply.github.com>
2023-11-10 22:44:22 +0900

    if文教えて

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests\LoginFromRequest;
 use Illuminate\Auth\EloquentUserProvider;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AuthController extends Controller
 {
@@ -32,14 +33,42 @@ class AuthController extends Controller
         // 別ページにバリデーション書いている場合
         $credentials = $request->validated();
 
-        // attrmptが認証関連全てやってる
+        $user = User::where('email',$credentials['email'])->first();
+        if(!is_null($user)){
+            if($user->locked_flg === 1){
+
+                return back()->withErrors([
+                    'login_error' => 'アカウントがロックされています。',
+                ]);
+            }
+
+  // attrmptが認証関連全てやってる
         // vendor/lalavel/src/Auth/SessionGurrd->attemptが何してるか書いてある
         // vendor/lalavel/src/Auth/EloquentUserProvider->↑確認したらこれも出てくる
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
 
+            if($user->error_count > 0){
+                $user->error_count = 0;
+                $user->save();
+            }
+
+
             return redirect()->route('home')->with('login_success','ログインが成功しました');
         }
+
+        $user->error_count =  $user->error_count +1;
+        
+        if($user->error_count > 5){
+            $user->locked_flg = 1;
+            $user->save();
+            return back()->withErrors([
+                'login_error' => '不正アクセスの可能性があります。あなたのアカウントはロックされました。運営までお問い合わせください。（03-xxxxxxxx）',
+            ]);
+        }
+        $user->save();
+        }
+
         return back()->withErrors([
             'login_error' => 'メールアドレスかパスワードが違います。',
         ]);
