4db742cda655e9e5f0600437e05d17c52c6a1e1a
yuya671023 <126576075+yuya671023@users.noreply.github.com>
2023-11-11 13:12:50 +0900

    ｗｗｗ

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -12,14 +12,14 @@ use App\Models\User;
 class AuthController extends Controller
 {
     // return view
-    public function show_login ()
+    public function show_login()
     {
         return view('login.login_form');
     }
 
     //request
     //  App\Http\Requests\LoginFromRequest;
-    public function login (LoginFromRequest $request)
+    public function login(LoginFromRequest $request)
     {
         // ここでバリデーションする
         // $credentials = $request->validate([
@@ -33,40 +33,40 @@ class AuthController extends Controller
         // 別ページにバリデーション書いている場合
         $credentials = $request->validated();
 
-        $user = User::where('email',$credentials['email'])->first();
-        if(!is_null($user)){
-            if($user->locked_flg === 1){
+        $user = User::where('email', $credentials['email'])->first();
+        if (!is_null($user)) {
+            if ($user->locked_flg === 1) {
 
                 return back()->withErrors([
                     'login_error' => 'アカウントがロックされています。',
                 ]);
             }
 
-  // attrmptが認証関連全てやってる
-        // vendor/lalavel/src/Auth/SessionGurrd->attemptが何してるか書いてある
-        // vendor/lalavel/src/Auth/EloquentUserProvider->↑確認したらこれも出てくる
-        if (Auth::attempt($credentials)) {
-            $request->session()->regenerate();
+            // attrmptが認証関連全てやってる
+            // vendor/lalavel/src/Auth/SessionGurrd->attemptが何してるか書いてある
+            // vendor/lalavel/src/Auth/EloquentUserProvider->↑確認したらこれも出てくる
+            if (Auth::attempt($credentials)) {
+                $request->session()->regenerate();
 
-            if($user->error_count > 0){
-                $user->error_count = 0;
-                $user->save();
+                if ($user->error_count > 0) {
+                    $user->error_count = 0;
+                    $user->save();
+                }
+
+
+                return redirect()->route('home')->with('login_success', 'ログインが成功しました');
             }
 
+            $user->error_count =  $user->error_count + 1;
 
-            return redirect()->route('home')->with('login_success','ログインが成功しました');
-        }
-
-        $user->error_count =  $user->error_count +1;
-        
-        if($user->error_count > 5){
-            $user->locked_flg = 1;
+            if ($user->error_count > 5) {
+                $user->locked_flg = 1;
+                $user->save();
+                return back()->withErrors([
+                    'login_error' => '不正アクセスの可能性があります。あなたのアカウントはロックされました。運営までお問い合わせください。（03-xxxxxxxx）',
+                ]);
+            }
             $user->save();
-            return back()->withErrors([
-                'login_error' => '不正アクセスの可能性があります。あなたのアカウントはロックされました。運営までお問い合わせください。（03-xxxxxxxx）',
-            ]);
-        }
-        $user->save();
         }
 
         return back()->withErrors([
@@ -75,20 +75,20 @@ class AuthController extends Controller
     }
 
 
-/**
- * ユーザーをアプリケーションからログアウトさせる
- *
- * @param  \Illuminate\Http\Request  $request
- * @return \Illuminate\Http\Response
- */
-public function logout(Request $request)
-{
-    Auth::logout();
+    /**
+     * ユーザーをアプリケーションからログアウトさせる
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+        Auth::logout();
 
-    $request->session()->invalidate();
+        $request->session()->invalidate();
 
-    $request->session()->regenerateToken();
+        $request->session()->regenerateToken();
 
-    return redirect()->route('show_login');
-}
+        return redirect()->route('show_login');
+    }
 }
